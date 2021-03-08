62ac8744a8b3ad79aef515dc598b411d9bacac27
AkiraTenchi <detektivconan18@gmx.de>
2021-03-08 02:03:40 +0100

    optimized multithreading

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from os.path import isdir
 import librosa, soundfile
 from pydub import AudioSegment
 import threading
+import multiprocessing
 
-
+# returns all the paths for the mp3 files as a list
 def loadFiles(path):
     dirList = listdir(path)
     songList = []
@@ -18,7 +19,15 @@ def loadFiles(path):
     return songList
 
 
+def pitchSongs(paths, steps):
+    for song in paths:
+        pitchSong(song, steps)
+    print("done Pitching")
+
+
+# pitch shift the song by the requested amount
 def pitchSong(path, steps):
+    print("pitching: " + path)
     y, sr = librosa.load(path)
     y_pitched = librosa.effects.pitch_shift(y, sr, n_steps=steps)
 
@@ -28,22 +37,41 @@ def pitchSong(path, steps):
 
     soundfile.write(path + ".wav", y_pitched, sr)
 
+    # convert the wav output to mp3
     sound = AudioSegment.from_wav(path + ".wav")
     sound.export(path + ".mp3", format="mp3")
 
 
 def main():
-    path = input("Please input Song Folder Path")
-    steps = input("By how much do you want to pitch the songs (-10, 10)")
-    songList = loadFiles(path)
 
-    for song in songList:
-        print(threading.active_count())
-        if threading.active_count() <= 2:
-            threading.Thread(target=pitchSong, args=(song, steps)).start()
-            continue
-        print("here")
-        pitchSong(song, steps)
+    # Necessary input + validation
+    path = input("Please input Song Folder Path")
+    try:
+        steps = int(input("By how much do you want to pitch the songs (-10, 10)"))
+        threads = int(input("How many threads do you want to use?"))
+    except Exception:
+        print("invalid input")
+        return
+
+    if (
+        not isdir(path)
+        or steps > 10
+        or steps < -10
+        or multiprocessing.cpu_count() - 2 < threads
+    ):
+        print("invalid input")
+        return
+
+    songList = loadFiles(path)
+    listChunks = int(len(songList) / threads)
+
+    for i in range(0, threads - 1):
+        threading.Thread(
+            target=pitchSongs,
+            args=(songList[i * listChunks : (i + 1) * listChunks], steps),
+        ).start()
+
+    pitchSongs(songList[(threads - 1) * listChunks : len(songList)], steps)
 
 
 main()
